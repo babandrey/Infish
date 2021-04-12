1ead28858bfe1df38ab091bd3751ae2ed9673a84
fkeyzuwu <fkeyzuwu@gmail.com>
2021-04-13 00:36:01 +0300

    Update FoodManager.cs

--- Assets/Scripts/Food/FoodManager.cs
+++ Assets/Scripts/Food/FoodManager.cs
@@ -19,24 +19,13 @@ public class FoodManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            bool isPointerOverGameObject = EventSystem.current.IsPointerOverGameObject();
-            IsGameObjectUI();
-
-            Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            position.z = 0;
-            Instantiate(currentFood.gameObject, position, currentFood.transform.rotation, transform.Find("Food"));
+            if (EventSystem.current.currentSelectedGameObject == null) //checks if we pressed a button
+            {
+                Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                position.z = 0;
+                Instantiate(currentFood.gameObject, position, currentFood.transform.rotation, transform.Find("Food"));
+            }
         }
     }
 
-    private void IsGameObjectUI()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
-            Debug.Log("Ray hit" + hit.transform.name);
-        }
-
-    }
 }
