fc348864bac26969ef13a835c837e9bf93826014
fkeyzuwu <fkeyzuwu@gmail.com>
2021-04-16 19:15:15 +0300

    gonna go object poolingW OOO

--- Assets/Scripts/Food/FoodManager.cs
+++ Assets/Scripts/Food/FoodManager.cs
@@ -10,10 +10,6 @@ public class FoodManager : MonoBehaviour
     [SerializeField] private Food[] foodList;
     private Food currentFood;
 
-    void Awake()
-    {
-        cam = Camera.main;
-    }
     // Start is called before the first frame update
     void Start()
     {
