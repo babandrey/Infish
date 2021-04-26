be8952687406a0ced92d2554142189885ab813cc
fkeyzuwu <fkeyzuwu@gmail.com>
2021-04-27 01:25:00 +0300

    made piranha prefab

--- Assets/Scripts/Fish/Piranha.cs
+++ Assets/Scripts/Fish/Piranha.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Piranha : MonoBehaviour
+public class Piranha : Fish
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
