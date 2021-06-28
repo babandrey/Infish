d73a2dd181766b8846644b53fa32f31b35ae45a5
fkeyzuwu <fkeyzuwu@gmail.com>
2021-06-28 16:18:13 +0300

    added so fishies spawn from top

--- Assets/Scripts/UI/BuyButtonMethods.cs
+++ Assets/Scripts/UI/BuyButtonMethods.cs
@@ -2,17 +2,26 @@ using UnityEngine;
 
 public static class BuyButtonMethods
 {
+    private static float maxFishHeight = 0;
     public static void SpawnFish(string tag, string goldAmount)
     {
         int amount = int.Parse(goldAmount);
 
         if(GoldManager.GoldAmount >= amount)
         {
-            Vector3 position = new Vector3(Utility.GenerateRandomVector3().x, Camera.main.ViewportToWorldPoint(new Vector3(0, Random.value)).y, 0);
+            if (maxFishHeight == 0)
+            {
+                float screenHeight = Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y;
+                RectTransform buttonGrid = GameObject.Find("Button Grid").GetComponent<RectTransform>();
+                maxFishHeight = screenHeight - buttonGrid.anchorMin.y * 2;
+            }
+
+            Vector3 position = new Vector3(Utility.GenerateRandomVector3().x, maxFishHeight, 0);
 
             ObjectPooler.Instance.SpawnFromPool(tag, position);
 
             //Add force down
+
             GoldManager.DecreaseGold(amount);
         }
     }
